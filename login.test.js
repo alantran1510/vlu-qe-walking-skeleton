cc26220db406b8c9d9900bfd44436c63c06f4610
alantran1510 <thanh.2474802010356@vanlanguni.vn>
2026-09-16 13:52:09 +0700

    Update login.test.js

--- login.test.js
+++ login.test.js
@@ -1,1 +1,13 @@
+const login = require("./login");
 
+test("Login successfully", () => {
+    expect(login("admin", "123")).toBe(true);
+});
+
+test("Login fails with wrong password", () => {
+    expect(login("admin", "1234")).toBe(false);
+});
+
+test("Login fails with wrong username", () => {
+    expect(login("user", "123")).toBe(false);
+});
